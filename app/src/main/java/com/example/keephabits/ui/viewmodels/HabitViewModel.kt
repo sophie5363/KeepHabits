93d15a2b275e07a18a26330a8bfd724e3673dcb1
Sophie JACQUOT <sophienanterre@gmail.com>
2021-02-21 18:11:50 +0100

    Ajout de l'appel des fonctions add, update, delete, deleteAll du repository dans le Viewmodel

--- app/src/main/java/com/example/keephabits/ui/viewmodels/HabitViewModel.kt
+++ app/src/main/java/com/example/keephabits/ui/viewmodels/HabitViewModel.kt
@@ -3,22 +3,49 @@ package com.example.keephabits.ui.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.example.keephabits.data.database.HabitDatabase
 import com.example.keephabits.data.models.Habit
 import com.example.keephabits.logic.repository.HabitRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
 
 @InternalCoroutinesApi
 class HabitViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository: HabitRepository
-    val getAllHabits : LiveData<List<Habit>>
+    val getAllHabits: LiveData<List<Habit>>
 
     init {
         val habitDao = HabitDatabase.getDatabase(application).habitDao()
         repository = HabitRepository(habitDao)
 
         getAllHabits = repository.getAllHabits
+    }
+
+    fun addHabit(habit: Habit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addHabit(habit)
+        }
+    }
+
+    fun updateHabit(habit: Habit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateHabit(habit)
+        }
+    }
+
+    fun deleteHabit(habit: Habit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteHabit(habit)
+        }
+    }
+
+    fun deleteAllHabits(habit: Habit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteAllHabits()
+        }
     }
 
 }
