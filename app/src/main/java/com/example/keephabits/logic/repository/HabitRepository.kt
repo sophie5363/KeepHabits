ef6727a091f52ce35755a93837b4dce8ca2a935a
Sophie JACQUOT <sophienanterre@gmail.com>
2021-02-21 17:58:37 +0100

    Création du repository et des suspend func pour ajouter, mettre à jour, effacer une habitude ou effacer toutes les habitudes

---
+++ app/src/main/java/com/example/keephabits/logic/repository/HabitRepository.kt
@@ -0,0 +1,29 @@
+package com.example.keephabits.logic.repository
+
+import androidx.lifecycle.LiveData
+import com.example.keephabits.data.models.Habit
+import com.example.keephabits.logic.dao.HabitDao
+
+class HabitRepository(private val habitDao: HabitDao) {
+
+    val getAllHabits: LiveData<List<Habit>> = habitDao.getAllHabits()
+
+    suspend fun addHabit(habit: Habit){
+        habitDao.addHabit(habit)
+    }
+
+    suspend fun updateHabit(habit: Habit){
+        habitDao.updateHabit(habit)
+    }
+
+    suspend fun deleteHabit(habit: Habit){
+        habitDao.deleteHabit(habit)
+    }
+
+    suspend fun deleteAllHabits(){
+        habitDao.deleteAll()
+    }
+
+
+
+}
